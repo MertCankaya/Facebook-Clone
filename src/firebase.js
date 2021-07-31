9124b7adfea580240393a824743ee93c250bb510
Mert Çankaya <86437425+MertCankaya@users.noreply.github.com>
2021-07-31 23:13:54 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBUVg_utXRCtYjeT7Lgv94h_br_JIH_kTM",
-    authDomain: "facebook-clone-826d8.firebaseapp.com",
-    projectId: "facebook-clone-826d8",
-    storageBucket: "facebook-clone-826d8.appspot.com",
-    messagingSenderId: "268373730756",
-    appId: "1:268373730756:web:347593675568fc6a603091",
-    measurementId: "G-JH14WTGH6D"
+    apiKey: "AIzaSyAOXzUdrmx84VUHYgKWkhLZzXE8LT3dXl0",
+    authDomain: "facebook-clone-project-17520.firebaseapp.com",
+    projectId: "facebook-clone-project-17520",
+    storageBucket: "facebook-clone-project-17520.appspot.com",
+    messagingSenderId: "422931550720",
+    appId: "1:422931550720:web:106a91f4e3c1c4441efee5",
+    measurementId: "G-RMKG0BEWFS"
 };
   
 const firebaseApp = firebase.initializeApp(firebaseConfig)
